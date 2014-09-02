26053731e201e368288897ffb1062f8a2a5bee88
h0x91b <h0x91b@gmail.com>
2014-09-02 13:42:40 +0300

    [+] crc16 from redis

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ var redis = new Redis({host:'127.0.0.1', port: 7001, onConnect: onConnect});
 
 function onConnect() {
 	console.log('connected');
+	console.log(redis.keyToSlot('123456789').toString(16));
 	redis.cmd(['get','hello'], function(e, d){
 		console.log(e,d);
 	});
